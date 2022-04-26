3ca7dc799adb92cb95764064bcc7ebff0541edea
Max Gaurav <max.gaurav@rubicotech.in>
2022-04-26 11:08:47 +0530

    fix: route list spec using removed service

--- src/cli-commands/commands/route-list/route-list.service.spec.ts
+++ src/cli-commands/commands/route-list/route-list.service.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RouteListService } from './route-list.service';
-import { DatabaseHelperService } from '../../services/database-helper/database-helper.service';
 import { Logger } from '@nestjs/common';
 
 describe('RouteListService', () => {
@@ -8,14 +7,7 @@ describe('RouteListService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        RouteListService,
-        {
-          provide: DatabaseHelperService,
-          useValue: {},
-        },
-        Logger,
-      ],
+      providers: [RouteListService, Logger],
     }).compile();
 
     service = module.get<RouteListService>(RouteListService);
