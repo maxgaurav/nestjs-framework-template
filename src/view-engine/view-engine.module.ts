9da4c28865af0aad0d1ccd74553448a9f6c5a05d
Max Gaurav <max.gaurav@rubicotech.in>
2022-07-01 14:31:57 +0530

    fix: build issue due to incorrect engine mapping add: build check to confirm all changes are valid

--- src/view-engine/view-engine.module.ts
+++ src/view-engine/view-engine.module.ts
@@ -1,11 +1,11 @@
 import { Global, Module, Provider } from '@nestjs/common';
 import { ViewEngineConfigService } from './services/view-engine-config/view-engine-config.service';
 import { VIEW_RENDER_ENGINE } from './constants';
-import * as hbs from 'hbs';
+import * as Twig from 'twig';
 
 const ViewEngineProvider: Provider = {
   provide: VIEW_RENDER_ENGINE,
-  useValue: hbs,
+  useValue: Twig,
 };
 
 @Global()
