59cbe66d4b250a1bec1f6b5068e5bcc94abc5123
Max Gaurav <max.gaurav@rubicotech.in>
2023-06-30 11:16:32 +0530

    add: exit handling for cli commands

--- src/cli.ts
+++ src/cli.ts
@@ -3,11 +3,14 @@ import { CommandModule, CommandService } from 'nestjs-command';
 import { AppModule } from './app.module';
 import { LoggingService } from './services/logging/logging.service';
 import { TinkerService } from './cli-commands/commands/tinker/tinker.service';
+import * as process from 'process';
 
 (async () => {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get<LoggingService>(LoggingService));
   await app.init();
   app.get(TinkerService).setContext(app);
-  app.select(CommandModule).get(CommandService).exec();
+  await app.select(CommandModule).get(CommandService).exec();
+  await app.close();
+  process.exit(0);
 })();
